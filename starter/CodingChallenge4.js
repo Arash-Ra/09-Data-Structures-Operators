f19936adc829312dd03c893d833ee982d02fd4c3
Arash-Ra <rahebiarash@gmail.com>
2021-10-12 22:07:09 -0400

    commit 2021-10-12-22-06

---
+++ starter/CodingChallenge4.js
@@ -0,0 +1,17 @@
+'use strict';
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+  const inputFromYou = document.querySelector('textarea').value;
+
+  const lower = inputFromYou.toLowerCase();
+  const splited = [lower.split('\n')];
+  const newText = [];
+  for (const chars of splited) {
+    newText.push(n.replace(chars[0], chars[0].toUpperCase()));
+    console.log(newText.join('\n'));
+  }
+});
